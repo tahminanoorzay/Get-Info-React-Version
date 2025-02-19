66166868f9b77050a806ea00fc4ce0a6f925ea05
TAHMINA NOORZAY <tahminanoorzay1380@gmail.com>
2025-02-19 21:33:55 +0430

    Edite css styles and add name,lastname and age components

--- src/index.js
+++ src/index.js
@@ -2,45 +2,152 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-function GetName() {
-  const [name, setName] = useState("");
-
-  function handlerInput() {
-    alert(name);
-  }
-
+function GetDetails({
+  name,
+  setName,
+  lastName,
+  setLastName,
+  age,
+  setAge,
+  handlerAdd,
+}) {
   return (
     <div className="container">
-      <div class="input-group">
-        <input
-          className="fInput"
-          placeholder="Enter Someone"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button className="aBtn" onClick={handlerInput}>
-          Add
-        </button>
-      </div>
+      <GetName name={name} setName={setName} handlerAdd={handlerAdd} />
+      <GetLastName
+        lastName={lastName}
+        setLastName={setLastName}
+        handlerAdd={handlerAdd}
+      />
+      <GetAge age={age} setAge={setAge} handlerAdd={handlerAdd} />
     </div>
   );
 }
 
-function ShowResult() {
+function GetName({ name, setName, handlerAdd }) {
   return (
-    <div className="showDetails">
-      <div></div>
-      <button>Delete</button>
+    <div className="input-group">
+      <input
+        className="fInput"
+        placeholder="Enter your name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button className="aBtn" onClick={handlerAdd}>
+        Add
+      </button>
+    </div>
+  );
+}
+
+function GetLastName({ lastName, setLastName, handlerAdd }) {
+  return (
+    <div className="input-group">
+      <input
+        className="fInput"
+        placeholder="Enter your last name"
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <button className="aBtn" onClick={handlerAdd}>
+        Add
+      </button>
+    </div>
+  );
+}
+
+function GetAge({ age, setAge, handlerAdd }) {
+  return (
+    <div className=" input-group">
+      <input
+        className="fInput"
+        placeholder="Enter your age"
+        type="number"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <button className="aBtn" onClick={handlerAdd}>
+        Add
+      </button>
+    </div>
+  );
+}
+
+function ShowResult({
+  name,
+  setName,
+  setDisplayName,
+  showResult,
+  setShowResult,
+}) {
+  function handlerDelate() {
+    setName("");
+    setDisplayName("");
+    setShowResult(false);
+  }
+
+  return (
+    <div className="">
+      {showResult && (
+        <div className="container">
+          <div className="showDetails">
+            <div>{name}</div>
+            <input
+              onClick={handlerDelate}
+              type="button"
+              className="dBtn"
+              value="Delete"
+            ></input>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 function App() {
+  const [name, setName] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [displayLastName, setDisplayLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [displayAge, setDisplayAge] = useState("");
+  const [showResult, setShowResult] = useState(false);
+
+  function handlerAdd() {
+    setDisplayName(name);
+    setName("");
+    setDisplayLastName(lastName);
+    setLastName("");
+    setDisplayAge(age);
+    setAge("");
+    setShowResult(true);
+  }
+
   return (
     <>
-      <GetName />
-      <ShowResult />
+      <GetDetails
+        name={name}
+        setName={setName}
+        lastName={lastName}
+        setLastName={setLastName}
+        age={age}
+        setAge={setAge}
+        handlerAdd={handlerAdd}
+      />
+      <ShowResult
+        name={displayName}
+        setName={setName}
+        setDisplayName={setDisplayName}
+        showResult={showResult}
+        setShowResult={setShowResult}
+        lastName={displayLastName}
+        setLastName={setLastName}
+        age={displayAge}
+        setAge={setAge}
+      />
     </>
   );
 }
