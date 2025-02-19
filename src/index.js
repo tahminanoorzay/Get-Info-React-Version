4c5a9badf7381cf21ce630599caa28c2ce144745
TAHMINA NOORZAY <tahminanoorzay1380@gmail.com>
2025-02-19 19:05:26 +0430

    Add css file and give name from input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-function App() {}
+function GetName() {
+  const [name, setName] = useState("");
+
+  function handlerInput() {
+    alert(name);
+  }
+
+  return (
+    <div className="container">
+      <div class="input-group">
+        <input
+          className="fInput"
+          placeholder="Enter Someone"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className="aBtn" onClick={handlerInput}>
+          Add
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function ShowResult() {
+  return (
+    <div className="showDetails">
+      <div></div>
+      <button>Delete</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <GetName />
+      <ShowResult />
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
